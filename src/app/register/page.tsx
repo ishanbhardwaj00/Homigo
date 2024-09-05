e1d3b1fa768264baa3073844bebd78ff94111475
Ishan Bhardwaj <ishan.x.bhardwaj@gmail.com>
2024-09-05 21:36:25 +0530

    registration completed

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { log } from 'console'
 import { Poppins } from 'next/font/google'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -46,6 +46,46 @@ const questionnare = [
     heading: 'Drinking Preference',
     type: 'radio',
     options: ['Social Drinker', 'Frequent Drinker', 'Teetotaller'],
+  },
+  {
+    id: 'guestPolicy',
+    heading: 'Guest Policy',
+    type: 'radio',
+    options: [
+      'Have Guests Over Often',
+      'Have Guests Over Occasionally',
+      'Have Guests Over Rarely',
+    ],
+  },
+  {
+    id: 'regionalBackground',
+    heading: 'Regional Background',
+    type: 'radio',
+    options: [
+      'North India',
+      'East India',
+      'West India',
+      'South India',
+      'North-East India',
+      'Central India',
+    ],
+  },
+  {
+    id: 'interests',
+    heading: 'Hobbies & Interests',
+    type: 'checkbox',
+    options: [
+      'Books',
+      'Movies',
+      'Gym',
+      'Traveling',
+      'Sports',
+      'Dance',
+      'Partying',
+      'Gaming',
+      'Music',
+      'Cooking',
+    ],
   },
 ]
 const ErrorMessage = ({ text }) => {
@@ -281,8 +321,8 @@ const StepFour = ({ setStep }: { setStep: any }) => {
     formState: { errors, isSubmitting },
   } = useForm()
   return (
-    <div className="flex flex-col items-center justify-center h-screen max-h-screen bg-bottom animateRegistration">
-      <div className="w-3/4  flex flex-col justify-between gap-16">
+    <div className="flex flex-col items-center justify-center bg-custom-pattern bg-no-repeat bg-center bg-cover animateRegistration overflow-scroll">
+      <div className="w-3/4  py-16 flex flex-col justify-between gap-8">
         <div className="flex flex-col gap-2">
           <div
             className={`${poppins.className} flex flex-col text-4xl font-bold text-primary `}
@@ -296,42 +336,312 @@ const StepFour = ({ setStep }: { setStep: any }) => {
           </p>
         </div>
         <form
-          className="flex flex-col gap-2"
-          onSubmit={handleSubmit((data) => console.log(data))}
+          className="flex flex-col gap-4"
+          onSubmit={handleSubmit((data) => {
+            setStep((step) => step + 1)
+            console.log(data)
+          })}
         >
           {questionnare.map((question) => (
-            <div className="flex flex-col capitalize gap-4">
-              <span className="font-semibold">{question.heading}</span>
+            <div key={question.id} className="flex flex-col capitalize gap-1">
+              <span className="font-semibold text-lg">{question.heading}</span>
               <div className="flex flex-row gap-2 align items-start flex-wrap">
                 {question.options.map((option, index) => (
                   <label
-                    className="flex text-sm items-center py-2 px-3 border-solid border-2 border-black rounded-full has-[:checked]:bg-green-300 has-[:checked]:border-0"
+                    key={option}
+                    className="flex text-sm items-center py-2 px-3 border-solid border border-black rounded-full has-[:checked]:bg-button-primary has-[:checked]:border-button-primary"
                     htmlFor={option}
                   >
                     <span className="w-max">{option}</span>
                     <input
+                      {...register(`${question.id}`, {
+                        required: 'This field is required',
+                      })}
                       className="hidden"
                       type={question.type}
+                      value={option}
                       name={question.id}
                       id={option}
                     />
                   </label>
                 ))}
               </div>
+              {errors[question.id] && (
+                <ErrorMessage text={errors[question.id]?.message} />
+              )}
             </div>
           ))}
+          <button className="w-full rounded-full bg-button-primary py-4 text-2xl font-bold text-primary mt-4">
+            Next
+          </button>
         </form>
       </div>
     </div>
   )
 }
+
+const locations = [
+  'Near Cyberhub',
+  'Near Unitech CyberPark',
+  'Near Golf Course Ext',
+  'Near Millenium City Centre',
+  'Near Old Gurgaon',
+  'Near IMT Manesar',
+  'Near Phase II',
+]
+const StepFive = ({ setStep }: { setStep: any }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors, isSubmitting },
+  } = useForm()
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-custom-pattern bg-no-repeat bg-center bg-cover animateRegistration overflow-scroll">
+      <div className="w-3/4  py-16 flex flex-col justify-between gap-14">
+        <div className="flex flex-col gap-2">
+          <div
+            className={`${poppins.className} flex flex-col text-4xl font-bold text-primary `}
+          >
+            <span>What are your</span>
+            <span>Roommate</span>
+            <span>Preferences?</span>
+          </div>
+        </div>
+        <form
+          className="flex flex-col gap-4"
+          onSubmit={handleSubmit((data) => {
+            setStep((step) => step + 1)
+            console.log(data)
+          })}
+        >
+          <div className="flex flex-col text-button-radio-button font-semibold gap-2">
+            <span>
+              <p className="text-sm">Where Would You Prefer To Stay?</p>
+              <p className="text-xs font-thin text-button-radio-button"></p>
+            </span>
+            <div>
+              {locations.map((location, index) => (
+                <label
+                  className="flex justify-start p-2 items-center gap-3"
+                  key={index}
+                  htmlFor={location}
+                >
+                  <input
+                    type="checkbox"
+                    className="appearance-none bg-transparent border border-solid border-gray-300 w-4 h-3 checked:border-primary"
+                    value={location}
+                    id={location}
+                    {...register('locationPreferences', {
+                      required: 'This field is required',
+                    })}
+                  />
+                  <div className="flex flex-col w-full gap-1">
+                    <span className="text-xs font-normal w-max">
+                      {location}
+                    </span>
+                    <div className="w-full h-px bg-gray-300 "></div>
+                  </div>
+                </label>
+              ))}
+              {errors.locationPreferences && (
+                <ErrorMessage text={errors.locationPreferences.message} />
+              )}
+            </div>
+          </div>
+          <div className="flex flex-col text-button-radio-button font-semibold gap-2">
+            <div className="flex flex-col gap-2">
+              <p className="text-sm">
+                Are you comfortable living with someone who consumes
+                non-vegetarian food?
+              </p>
+              <p className="text-xs font-thin text-button-radio-button"></p>
+              <div className="flex gap-2 flex-wrap ">
+                {[
+                  'Yes, I’m comfortable with it',
+                  'No, I prefer a vegetarian-only environment',
+                ].map((option, index) => (
+                  <label
+                    key={option}
+                    className="flex text-sm items-center font-normal py-2 px-3 border-solid border border-black rounded-full has-[:checked]:bg-button-primary has-[:checked]:border-button-primary"
+                    htmlFor={option}
+                  >
+                    <span className="w-max">{option}</span>
+                    <input
+                      className="hidden"
+                      type="radio"
+                      value={option}
+                      name="nonVegPreference"
+                      id={option}
+                      {...register(`nonVegPreference`, {
+                        required: 'This field is required',
+                      })}
+                    />
+                  </label>
+                ))}
+                {errors.nonVegPreference && (
+                  <ErrorMessage text={errors.nonVegPreference.message} />
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col text-button-radio-button font-semibold gap-2">
+            <div className="flex flex-col gap-2">
+              <p className="text-sm">What is your preferred lease duration?</p>
+              <p className="text-xs font-thin text-button-radio-button"></p>
+              <div className="flex gap-2 flex-wrap ">
+                {['No lease', '3 months', '6 months', '12 months'].map(
+                  (option, index) => (
+                    <label
+                      key={option}
+                      className="flex text-sm items-center font-normal py-2 px-3 border-solid border border-black rounded-full has-[:checked]:bg-button-primary has-[:checked]:border-button-primary"
+                      htmlFor={option}
+                    >
+                      <span className="w-max">{option}</span>
+                      <input
+                        {...register(`lease`, {
+                          required: 'This field is required',
+                        })}
+                        className="hidden"
+                        type="radio"
+                        value={option}
+                        name="lease"
+                        id={option}
+                      />
+                    </label>
+                  )
+                )}
+              </div>
+              {errors.lease && <ErrorMessage text={errors.lease.message} />}
+            </div>
+          </div>
+          <button className="w-full rounded-full bg-button-primary py-4 text-2xl font-bold text-primary mt-8">
+            Next
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+const StepSix = ({ setStep }: { setStep: any }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors, isSubmitting },
+  } = useForm()
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0]
+    if (file) {
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        setImage(reader.result)
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+  const [image, setImage] = useState(null)
+  const router = useRouter()
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-custom-pattern bg-no-repeat bg-center bg-cover animateRegistration overflow-scroll">
+      <div className="w-3/4  py-12 flex flex-col justify-between gap-12">
+        <div className="flex flex-col gap-2">
+          <div
+            className={`${poppins.className} flex flex-col text-4xl font-bold text-primary `}
+          >
+            <span>We're Almost</span>
+            <span>There</span>
+          </div>
+          <p className="text-primary">Let's put the final piece in place</p>
+        </div>
+        <form
+          className="flex flex-col gap-4"
+          onSubmit={handleSubmit((data) => {
+            console.log(data)
+            router.push('/')
+          })}
+        >
+          <div className="avatar-container self-center">
+            <input
+              {...register('image', { required: 'Image is required' })}
+              type="file"
+              accept="image/*"
+              id="file-input"
+              style={{ display: 'none' }}
+              onChange={handleFileChange}
+            />
+            <label
+              htmlFor="file-input"
+              className="avatar-label border-primary border-2"
+            >
+              {image ? (
+                <img src={image} alt="Avatar" className="avatar-image" />
+              ) : (
+                <div className="flex avatar-placeholder text-base">
+                  <span className="text-3xl font-bold">+</span>
+                  <span>Upload Your Picture</span>
+                </div>
+              )}
+            </label>
+          </div>
+          {errors.image && <ErrorMessage text={errors.image.message} />}
+
+          <div className="flex flex-col gap-2">
+            <p className="text-base font-semibold text-button-radio-button ml-2">
+              Bio*
+            </p>
+            <div className="flex justify-center items-center">
+              <textarea
+                {...register('bio', { required: 'This field is required' })}
+                className="w-full h-36 border rounded-lg p-3 text-base border-button-radio-button focus:border-blue-500 active:ring-blue-500 transition duration-300 ease-in-out"
+                placeholder="Tell us about you – your vibe, your quirks, and what makes you a great roommate!"
+              />
+            </div>
+          </div>
+          {errors.bio && <ErrorMessage text={errors.bio.message} />}
+
+          <div className="flex flex-col gap-2">
+            <p className="text-base font-semibold text-button-radio-button ml-2">
+              Monthly Rent Preferences
+            </p>
+            <div className="flex">
+              <input
+                {...register('monthlyRentPreferences', {
+                  required: 'This field is required',
+                  validate: (data) => {
+                    return !isNaN(data) || 'Should be a number'
+                  },
+                })}
+                className="w-full border rounded-lg p-3 text-base border-button-radio-button focus:border-blue-500 active:ring-blue-500 transition duration-300 ease-in-out"
+                placeholder="Enter A Number"
+              />
+            </div>
+            {errors.monthlyRentPreferences && (
+              <ErrorMessage text={errors.monthlyRentPreferences.message} />
+            )}
+          </div>
+          <button className="w-full rounded-full bg-button-primary py-4 text-2xl font-bold text-primary mt-8">
+            Start Matching
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
 const page = () => {
-  const [step, setStep] = useState<number>(4)
+  const [step, setStep] = useState<number>(1)
 
   if (step === 1) return <StepOne setStep={setStep} />
   else if (step === 2) return <StepTwo setStep={setStep} />
   else if (step == 3) return <StepThree setStep={setStep} />
   else if (step === 4) return <StepFour setStep={setStep} />
+  else if (step === 5) return <StepFive setStep={setStep} />
+  else if (step === 6) return <StepSix setStep={setStep} />
 }
 
 export default page
