99e546307167100425d5f1c04cb40fbeda3d497d
Ishan Bhardwaj <Ishan.Bhardwaj@hsc.com>
2024-09-15 16:37:57 +0530

    chat list created

---
+++ frontend/src/containers/Chat.tsx
@@ -0,0 +1,85 @@
+import { useNavigate } from 'react-router-dom'
+
+const Chats = () => {
+  const navigate = useNavigate()
+  return (
+    <div className="flex flex-col flex-1 gap-2">
+      <div className="flex flex-col py-7 px-7 gap-5">
+        <span className="capitalize text-lg text-gray-dark font-medium">
+          queued (2)
+        </span>
+        <div className="flex gap-4">
+          <span className="border-2 border-primary solid rounded-full h-24 w-24">
+            <img
+              src="/images/matched_dp.jpg"
+              className="h-full w-full bg-cover rounded-full"
+            />
+          </span>
+          <span className="border-2 border-primary solid rounded-full h-24 w-24">
+            <img
+              src="/images/matched_dp.jpg"
+              className="h-full w-full bg-cover rounded-full"
+            />
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <span className="capitalize text-gray-dark text-lg font-medium px-7">
+          recents
+        </span>
+        {/* cards */}
+        <div className="flex flex-col gap-2 p-1">
+          <div
+            onClick={() => {
+              navigate('/chats/1')
+            }}
+            className="flex  w-full p-1 px-7 gap-3 active:bg-slate-200 rounded-2xl"
+          >
+            <span className="rounded-full h-20 w-20">
+              <img
+                src="/images/matched_dp.jpg"
+                className="h-full w-full bg-cover rounded-full"
+              />
+            </span>
+            <div className="flex flex-col justify-center">
+              <span className="capitalize text-xl font-medium">ishan</span>
+              <span className="text-gray-dark text-lg text-light">
+                you're cool
+              </span>
+            </div>
+          </div>
+          <div className="flex  w-full p-1 px-7 gap-3">
+            <span className="rounded-full h-20 w-20">
+              <img
+                src="/images/matched_dp.jpg"
+                className="h-full w-full bg-cover rounded-full"
+              />
+            </span>
+            <div className="flex flex-col justify-center">
+              <span className="capitalize text-xl font-medium">ishan</span>
+              <span className="text-gray-dark text-lg text-light">
+                you're cool
+              </span>
+            </div>
+          </div>
+          <div className="flex  w-full p-1 px-7 gap-3">
+            <span className="rounded-full h-20 w-20">
+              <img
+                src="/images/matched_dp.jpg"
+                className="h-full w-full bg-cover rounded-full"
+              />
+            </span>
+            <div className="flex flex-col justify-center">
+              <span className="capitalize text-xl font-medium">ishan</span>
+              <span className="text-gray-dark text-lg text-light">
+                you're cool
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Chats
