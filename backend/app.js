43444315919b05eb7d847bfaeefb21e2d6992e73
Ishan Bhardwaj <ishan.x.bhardwaj@gmail.com>
2024-09-20 15:01:44 +0530

    redis-based mq for saving user mesages, script added in package.json, removed dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,11 @@
 import express from 'express'
-import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import mongoose from 'mongoose'
+import mongoose, { mongo } from 'mongoose'
 import axios from 'axios'
 import http from 'http'
 import { createSocketServer } from './sockets/socket.js'
 import Chat from './models/chat.model.js'
-
-// Load environment variables
-dotenv.config()
 
 // Middleware
 const app = express()
@@ -35,44 +31,46 @@ app.get('/', (req, res) => {
 
 // Get all users (requires authentication)
 app.get('/users', verifyJwt, async (req, res) => {
-    const users = await User.find({})
-    return res.json({
-      success: true,
-      users,
-    })
+  const users = await User.find({})
+  return res.json({
+    success: true,
+    users,
   })
+})
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected!'))
   .catch((err) => console.error('MongoDB connection error:', err))
-
-
-
 
 const PORT = process.env.PORT || 3000
 
 app.get('/chats', verifyJwt, async (req, res) => {
-  console.log(req.decodedToken)
+  // console.log(req.decodedToken)
+  const userId = new mongoose.Types.ObjectId(req.decodedToken._id)
+  console.log(userId)
+
   const userChats = await Chat.find({
-    recipients: { $all: [req.decodedToken._id] },
+    recipients: { $all: [userId] },
   })
     .populate('messages')
     .populate({ path: 'recipients', select: { metaDat: 1, userDetails: 1 } })
+  console.log(userChats)
 
   const formattedChats = userChats.map((chat) => {
     return {
       ...chat._doc, // Spread the rest of the chat document
-      recipients: chat.recipients.filter(
-        (recipientId) => !recipientId.equals(req.decodedToken._id)
-      ), // Exclude current user's ID
+      recipients: chat.recipients.filter((recipient) => {
+        console.log(recipient._id.toString())
+        console.log(userId.toString())
+
+        return recipient._id.toString() !== userId.toString() // Exclude current user's ID
+      }), // Exclude current user's ID
     }
   })
 
+  console.log(formattedChats)
   res.json({ success: true, chats: formattedChats })
 })
 
